87f04ccb4a8f2b75715e373d7795f06c02d2fa78
Aleena-Yameen <aleenayameen50@gmail.com>
2026-07-30 17:26:52 +0500

    feat: add dynamic navbar title

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 export default function Navbar() {
+  const pathname = usePathname();
+  const currentPage = pathname.split("/")[1];
+
+const pageTitle =
+  currentPage.charAt(0).toUpperCase() +
+  currentPage.slice(1);
   return (
     <header className="h-16 border-b border-slate-800 bg-slate-900 flex items-center justify-between px-8">
       <h2 className="text-xl font-semibold">
-        Dashboard
-      </h2>
+  {pageTitle}
+</h2>
 
       <div className="flex items-center gap-4">
         <button className="text-xl">🔔</button>
